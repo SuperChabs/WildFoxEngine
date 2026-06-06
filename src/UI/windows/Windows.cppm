fdb9c764f5f4556853d36f17831cf4eddd0853ff
SuperChabs <tumir455@gmail.com>
2026-06-06 23:19:18 +0200

    rewrite all modules into headers and somewhere also into realizations

--- src/UI/windows/Windows.cppm
+++
@@ -1,8 +0,0 @@
-export module WFE.UI.Windows;
-
-export import WFE.UI.Windows.ViewportWindow;
-export import WFE.UI.Windows.MenuBarWindow;
-export import WFE.UI.Windows.HierarchyWindow;
-export import WFE.UI.Windows.PropertiesWindow;
-export import WFE.UI.Windows.InspectorWindow;
-export import WFE.UI.Windows.ConsoleWindow;
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,20 @@
-import WFE.Engine;
-import WFE.Core.Logger;
+#include "engine/Engine.h"
+#include "core/logging/Logger.h"
 
-int main() 
-{
+int main() {
 #if defined (__WIN32__)
     exit()
 #endif
 
     Engine e(1020, 800, "Allusion");
-    
-    if (!e.Initialize()) 
-    {
+
+    if (!e.Initialize()) {
         Logger::Log(LogLevel::ERROR, "Failed to initialize game");
         return -1;
     }
-    
+
     e.Run();
     e.Shutdown();
-    
+
     return 0;
 }
